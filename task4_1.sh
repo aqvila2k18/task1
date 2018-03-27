f673f0aefa83fc5b51a061005a2e2b92c50af0cb
root <aqvila.true@gmail.com>
2018-03-28 00:04:32 +0300

    System Info script

--- task4_1.sh
+++ task4_1.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 cpuinf=$(dmidecode -s processor-version)
+meminfo=$(free -h | grep 'Mem:' | awk  '{print $2}')
+bbpminfo=$(dmidecode -s  baseboard-product-name)
+bbminfo=$(dmidecode -s  baseboard-manufacturer)
 echo '---Hardware---'>task4_1.out
-echo 'CPU:'${cpuinf:-'Uknown'}>>task4_1.out
+echo 'CPU: '${cpuinf:-'Uknown'}>>task4_1.out
+echo 'RAM: '${meminfo:-'Uknown'}>>task4_1.out
+echo 'Motherboard: '${bbpminfo:-'Uknown'}'/'${bbminfo:-'Uknown'}>>task4_1.out
