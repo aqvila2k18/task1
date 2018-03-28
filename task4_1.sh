86464aeb9fadcd47c3c2e49907ea8e4273ad4a27
root <aqvila.true@gmail.com>
2018-03-28 17:46:00 +0300

    script

--- task4_1.sh
+++ task4_1.sh
@@ -3,7 +3,7 @@
 apt-get install bc  > /dev/null 2>&1
 
 #Get system info
-cpuinf=$(dmidecode -s processor-version)
+cpuinf=$(cat /proc/cpuinfo | grep 'model name' -m 1 | awk -F": " '{print $2}')
 meminfo=$(free -h | grep 'Mem:' | awk  '{print $2}')
 bbpminfo=$(dmidecode -s baseboard-product-name)
 bbminfo=$(dmidecode -s baseboard-manufacturer)
@@ -11,18 +11,18 @@ ssninfo=$(dmidecode -s system-serial-number)
 osd=$(cat /etc/lsb-release | grep 'DISTRIB_DESCRIPTION' | awk -F"=" '{print $2}' | awk -F "\"" '{print $2}')
 instdate=$(uname -r)
 hostname=$(hostname)
-upsec=$(cat /proc/uptime | awk -F"." '{print $1}')
-let "updaywhole = upsec / 86400"
-let "updayfrac = upsec % 86400"
+uptime=$(uptime  -p | awk -F"up " '{print $2}')
+procrun=$(ps axuh | wc -l)
 
 #Put sistem info to task4_1.out
 echo '---Hardware---'>task4_1.out
 echo 'CPU: '${cpuinf:-'Uknown'}>>task4_1.out
 echo 'RAM: '${meminfo:-'Uknown'}>>task4_1.out
-echo 'Motherboard: '${bbpminfo:-'Uknown'}'/'${bbminfo:-'Uknown'}>>task4_1.out
+echo 'Motherboard: '${bbminfo:-'Uknown'}' '${bbpminfo:-'Uknown'}>>task4_1.out
 echo 'System Serial Number: '${ssninfo:-'Uknown'}>>task4_1.out
 echo '---System---'>>task4_1.out
 echo 'OS Distribution: '${osd:-'Uknown'}>>task4_1.out
 echo 'Kernel version: '${instdate:-'Uknown'}>>task4_1.out
 echo 'Hostname: '${hostname:-'Uknown'}>>task4_1.out
-echo 'Uptime: '$updaywhole'.'$updayfrac'days'>>task4_1.out
+echo 'Uptime: '$uptime>>task4_1.out
+echo 'Processes running: '$procrun>>task4_1.out
